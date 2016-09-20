554c0223a06c4f3df4e5181a891f865814a68739
John C. Daub <hsoi@hsoi.com>
2016-09-19 19:41:48 -0500

    updating docs script in hopes that makes jazzy happier.

--- Support/create_docs.sh
+++ Support/create_docs.sh
@@ -5,7 +5,10 @@ echo -e "Executing create_docs"
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
     echo -e "Generating Jazzy output \n"
     
-    jazzy --clean --author "Oven Bits, LLC" --author_url https://ovenbits.com --github_url https://github.com/ovenbits/Alexandria --xcodebuild-arguments "-scheme,Alexandria" --module Alexandria --root-url https://ovenbits.github.io/Alexandria --theme apple --swift-version 3.0
+    # Hsoi 2016-09-19 - https://github.com/realm/jazzy/issues/656 removing --swift-version to avoid problems.
+    # jazzy --clean --author "Oven Bits, LLC" --author_url https://ovenbits.com --github_url https://github.com/ovenbits/Alexandria --xcodebuild-arguments "-scheme,Alexandria" --module Alexandria --root-url https://ovenbits.github.io/Alexandria --theme apple --swift-version 3.0
+    jazzy --clean --author "Oven Bits, LLC" --author_url https://ovenbits.com --github_url https://github.com/ovenbits/Alexandria --xcodebuild-arguments "-scheme,Alexandria" --module Alexandria --root-url https://ovenbits.github.io/Alexandria --theme apple
+    
 
     pushd docs
 
